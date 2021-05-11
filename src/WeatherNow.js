d6ccec343d5a1ed23f161335b4d9aa1b62f407d7
hanksbonniec <hanksbonniec@gmail.com>
2021-05-11 12:03:25 -0700

    Adjusted spacing and icons

--- src/WeatherNow.js
+++ src/WeatherNow.js
@@ -6,31 +6,30 @@ import WeatherIcon from "./WeatherIcon";
 export default function WeatherNow(props) {
   return (
     <div className="WeatherNow">
+      <h1>{props.data.city}</h1>
       <div className="row align-items-center">
-          <div className="col-8">
-            <h1>{props.data.city}</h1>
+          <div className="col-4">
             <ul>
               <li>
                 <FormattedDate date={props.data.date} />
               </li>
               <li>{props.data.condition}</li>
             </ul>
-            <br />
-            <div className="clearfix">
+          </div>
+          <div className="col-4 clearfix">
               <div className="float-left">
                 <WeatherIcon code={props.data.icon} alt={props.data.description}/>
               </div> 
               <Temperature celsius={props.data.temperature} />
             </div>
-          </div>
           <div className="col-4">
             <ul>
               <li>
-                <span role="img">💧</span> <span></span>
+                <i class="fas fa-tint"></i> <span></span>
                 {props.data.humidity}%
               </li>
               <li>
-                <span role="img">🌬️</span> <span></span> {props.data.wind} km/h
+                <i class="fas fa-wind"></i> <span></span> {props.data.wind} km/h
               </li>
             </ul>
           </div>
